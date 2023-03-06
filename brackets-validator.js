49924809dc146350a7b3901a9fc6125422f53faf
Héliton Gustavo <45669416+gustavoC137@users.noreply.github.com>
2023-03-06 15:07:22 -0300

    comments

--- brackets-validator.js
+++ brackets-validator.js
@@ -1,3 +1,7 @@
+/*
+* A regex is applied on each iteration and the pattern found is stripped from the string.
+* If at the end of the loop the string is empty, true is returned to the validator.
+* */
 function validarOrdemDosColchetesRegex(string) {
   const regex = /(\(\)|\{\}|\[\])+/;
   while (regex.test(string)) {
